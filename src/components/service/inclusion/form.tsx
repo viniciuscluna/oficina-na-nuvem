d187f8a7368b68786103502137724462eafabb18
Vinicius Luna <viniciuscluna@gmail.com>
2023-07-19 23:30:13 -0300

    Fix layout for Include prestacao serviço

--- src/components/service/inclusion/form.tsx
+++ src/components/service/inclusion/form.tsx
@@ -54,138 +54,140 @@ const Form = ({
   return (
     <form onSubmit={handleSubmit(submit)}>
       <div className="space-y-4">
-        <div>
-          <label
-            htmlFor="prestadorId"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Prestador
-          </label>
-          <select
-            id="prestadorId"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            {...register("prestadorId")}
-          >
-            {prestadores.map((prestador, index) => (
-              <option key={index} value={prestador.id}>
-                {prestador.nome}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label
-            htmlFor="clienteId"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Cliente
-          </label>
-          <select
-            id="clienteId"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            {...register("clienteId")}
-          >
-            {clientes.map((cliente, index) => (
-              <option key={index} value={cliente.id}>
-                {cliente.nome}
-              </option>
-            ))}
-            <option value="other">Incluir</option>
-          </select>
-          {showClienteForm ? <ClienteForm register={register} /> : <></>}
-        </div>
-        <div>
-          <label
-            htmlFor="veiculoId"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Veículo
-          </label>
-          <select
-            id="veiculoId"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            {...register("veiculoId")}
-          >
-            {veiculos.map((veiculo, index) => (
-              <option key={index} value={veiculo.id}>
-                {veiculo.marca} - {veiculo.modelo}
-              </option>
-            ))}
-            <option value="other">Incluir</option>
-          </select>
-          {showVeiculoForm ? (
-            <VeiculoForm marcas={marcas} register={register} watch={watch} />
-          ) : (
-            <></>
-          )}
-        </div>
-        <div>
-          <label
-            htmlFor="servicos"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Serviços
-          </label>
-          <div className="flex gap-2 flex-col">
-            {servicos.map((servico, index) => (
-              <div
-                key={index}
-                className="flex w-full justify-between flex-wrap border border-gray-600 rounded-lg p-4"
-              >
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Categoria do Serviço
-                  </label>
-                  <select
-                    className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    {...register(`servicos.${index}.subServicoId`)}
-                  >
-                    {subServicos?.map((subServico, index) => (
-                      <option key={index} value={subServico.id}>
-                        {subServico.titulo} - {subServico.desc} -{" "}
-                        {subServico.categoria.titulo}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Valor
-                  </label>
-                  <input
-                    type="number"
-                    id="small-input"
-                    className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    {...register(`servicos.${index}.valor`)}
-                  ></input>
-                </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Descrição Serviço
-                  </label>
-                  <textarea
-                    id="small-input"
-                    className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    {...register(`servicos.${index}.nome`)}
-                  ></textarea>
-                </div>
-                <button
-                  type="button"
-                  onClick={() => removeServico(index)}
-                  className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Remover
-                </button>
-              </div>
-            ))}
+        <div className="overflow-auto max-h-[80dvh]">
+          <div>
+            <label
+              htmlFor="prestadorId"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Prestador
+            </label>
+            <select
+              id="prestadorId"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              {...register("prestadorId")}
+            >
+              {prestadores.map((prestador, index) => (
+                <option key={index} value={prestador.id}>
+                  {prestador.nome}
+                </option>
+              ))}
+            </select>
           </div>
-          <button
-            type="button"
-            onClick={() => addServico()}
-            className="px-3 py-2 my-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Adicionar Serviço
-          </button>
+          <div>
+            <label
+              htmlFor="clienteId"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Cliente
+            </label>
+            <select
+              id="clienteId"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              {...register("clienteId")}
+            >
+              {clientes.map((cliente, index) => (
+                <option key={index} value={cliente.id}>
+                  {cliente.nome}
+                </option>
+              ))}
+              <option value="other">Incluir</option>
+            </select>
+            {showClienteForm ? <ClienteForm register={register} /> : <></>}
+          </div>
+          <div>
+            <label
+              htmlFor="veiculoId"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Veículo
+            </label>
+            <select
+              id="veiculoId"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              {...register("veiculoId")}
+            >
+              {veiculos.map((veiculo, index) => (
+                <option key={index} value={veiculo.id}>
+                  {veiculo.marca} - {veiculo.modelo}
+                </option>
+              ))}
+              <option value="other">Incluir</option>
+            </select>
+            {showVeiculoForm ? (
+              <VeiculoForm marcas={marcas} register={register} watch={watch} />
+            ) : (
+              <></>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="servicos"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Serviços
+            </label>
+            <div className="flex gap-2 flex-col">
+              {servicos.map((servico, index) => (
+                <div
+                  key={index}
+                  className="flex w-full justify-between flex-wrap border border-gray-600 rounded-lg p-4"
+                >
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Categoria do Serviço
+                    </label>
+                    <select
+                      className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      {...register(`servicos.${index}.subServicoId`)}
+                    >
+                      {subServicos?.map((subServico, index) => (
+                        <option key={index} value={subServico.id}>
+                          {subServico.titulo} - {subServico.desc} -{" "}
+                          {subServico.categoria.titulo}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Valor
+                    </label>
+                    <input
+                      type="number"
+                      id="small-input"
+                      className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      {...register(`servicos.${index}.valor`)}
+                    ></input>
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Descrição Serviço
+                    </label>
+                    <textarea
+                      id="small-input"
+                      className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      {...register(`servicos.${index}.nome`)}
+                    ></textarea>
+                  </div>
+                  <button
+                    type="button"
+                    onClick={() => removeServico(index)}
+                    className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Remover
+                  </button>
+                </div>
+              ))}
+            </div>
+            <button
+              type="button"
+              onClick={() => addServico()}
+              className="px-3 py-2 my-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Adicionar Serviço
+            </button>
+          </div>
         </div>
         <div className="bottom-0 left-0 flex justify-center w-full pb-4 space-x-4 md:px-4 md:absolute">
           <button

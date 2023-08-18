c921b95a5fda482f38ffd109e1e1eab1fa724d83
Vinicius Luna <viniciuscluna@gmail.com>
2023-08-18 01:36:11 -0300

    wip

--- src/utils/prestacaoServicoGroup.ts
+++ src/utils/prestacaoServicoGroup.ts
@@ -28,7 +28,65 @@ export const returnGroupedPrestacaoServico = (
   return prestacao;
 };
 
+export const syncGroupedWithProducts = (
+  prestacaoServico: PrestacaoServico
+): PrestacaoServico => {
+  const groups = prestacaoServico.groupedProducts;
+  const products = prestacaoServico.produtos;
 
-export const syncGroupedWithProducts = (prestacaoServico: PrestacaoServico) : PrestacaoServico => {
+  //Operation From Group
+  groups.forEach((group) => {
+    const productsFromGroup = products.filter((f) => f.nome == group.nome);
+    //Add
+    if (group.qtd > productsFromGroup.length) {
+      const qtd = group.qtd - productsFromGroup.length;
+      for (let index = 0; index < qtd; index++) {
+        products.push({
+          ...group,
+          qtd: 1,
+          id: undefined,
+        });
+      }
+    }
+
+    //Delete
+    if (group.qtd < productsFromGroup.length) {
+      const qtd = productsFromGroup.length - group.qtd;
+      for (let index = 0; index < qtd; index++) {
+        const idx = products.findIndex((f) => f.nome === group.nome);
+        products.splice(idx, 1);
+      }
+    }
+
+    //Update
+    products.forEach((product) => {
+      if (product.nome === group.nome) {
+        product.marca = group.marca;
+        product.modelo = group.modelo;
+        product.data_validade = group.data_validade;
+        product.garantia = group.garantia;
+        product.valor_Compra = group.valor_Compra;
+        product.valor_Venda = group.valor_Venda;
+      }
+    });
+  });
+
+  //Orphan Products
+  const names = Array.from(
+    new Set(
+      products
+        .map((f) => f.nome)
+        .filter((f) => !groups.map((m) => m.nome).includes(f))
+    )
+  );
+
+  names.forEach((item) => {
+    const qtd = products.filter((f) => f.nome === item).length;
+    for (let index = 0; index < qtd; index++) {
+      const idx = products.findIndex((f) => f.nome === item);
+      products.splice(idx, 1);
+    }
+  });
+
   return prestacaoServico;
-}
+};

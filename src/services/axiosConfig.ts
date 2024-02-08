3167c19961520a368fac8a7e817c75e6c95477a4
Vinicius Luna <viniciuscluna@gmail.com>
2024-02-08 20:29:06 -0300

    feat: update endpoint

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -11,7 +11,7 @@ export const instanceApi = axios.create({
 });
 
 export const instanceAuthApi = axios.create({
-  baseURL: import.meta.env.PROD ? "prestacao-nuvem-auth-api.azurewebsites.net/api" : "http://localhost:7056/api",
+  baseURL: import.meta.env.PROD ? "https://prestacao-nuvem-auth-api.azurewebsites.net/api" : "http://localhost:7056/api",
 });
 
 export const instanceFipe = axios.create({
